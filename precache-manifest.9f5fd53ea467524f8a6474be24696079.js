7f56fb056032c53833981ef5a84b7e733d263d76
KarthikMydur <30320307+KarthikMydur@users.noreply.github.com>
2020-04-23 14:48:51 +0530

    Updates

---
+++ precache-manifest.9f5fd53ea467524f8a6474be24696079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3135705a3ab8ca22f19f21344105d712",
+    "url": "/food-recipe-app/index.html"
+  },
+  {
+    "revision": "743cd305eb56584a166e",
+    "url": "/food-recipe-app/static/css/main.94280442.chunk.css"
+  },
+  {
+    "revision": "f91ab7ef91f23e3b325c",
+    "url": "/food-recipe-app/static/js/2.2c1a7c0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe-app/static/js/2.2c1a7c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743cd305eb56584a166e",
+    "url": "/food-recipe-app/static/js/main.105f5655.chunk.js"
+  },
+  {
+    "revision": "4583caea5fbc6418c63b",
+    "url": "/food-recipe-app/static/js/runtime-main.c762d519.js"
+  }
+]);
